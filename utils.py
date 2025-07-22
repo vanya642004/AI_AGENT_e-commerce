3d470d8cae9d692f2df2838fc3ba989b96b0e0b5
vanya642004 <139208978+vanya642004@users.noreply.github.com>
2025-07-22 12:55:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 
+
 def display_chart(df: pd.DataFrame, title: str = None):
+    if df.empty:
+        return
     fig, ax = plt.subplots()
-    df.plot(ax=ax)
+    df.select_dtypes(include=["number"]).plot(ax=ax)
     if title:
         ax.set_title(title)
     st.pyplot(fig)

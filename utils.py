fe3f2cafad31df1bc4de602ad610ca9f97d8a49c
vanya642004 <139208978+vanya642004@users.noreply.github.com>
2025-07-22 12:01:51 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,11 +2,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 
-
 def display_chart(df: pd.DataFrame, title: str = None):
-    """
-    Simple line chart wrapper: plots numeric columns vs index.
-    """
     fig, ax = plt.subplots()
     df.plot(ax=ax)
     if title:
